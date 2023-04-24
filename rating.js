6b2f29549a13bb60d24f0a45fccd82822a8ee7c7
Vsevolod Lazuka <74600980+arigandores@users.noreply.github.com>
2023-04-25 00:46:32 +0300

    Update rating.js

--- rating.js
+++ rating.js
@@ -97,8 +97,9 @@ window.onload = async function() {
             let color = null;
             let fontSize = null;
             let strippedRatingCount = ratingCount.replace(/\s/g, '');
-            span.innerHTML = `<b>${rating} - ${ratingCount}</b>`;
-            if (strippedRatingCount > 1000) {
+            span.innerHTML = `<b
+            ${rating} - ${ratingCount}</b>`;
+            if (strippedRatingCount >= 1000) {
                 color = 'black'; // highlight yellow if ratingCount is greater than 1000
                 fontSize = "20px";
             } else if (strippedRatingCount < 1000) {
